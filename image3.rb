0bc1b35c458e1e17603fbc043b681bdcfd2fa814
farhanprasla10 <farhan.prasla31@gmail.com>
2019-06-02 20:25:51 +0000

    challenges

---
+++ image3.rb
@@ -0,0 +1,50 @@
+class Image
+
+  def initialize(array)
+  @array = array
+  end
+
+
+  def output_image
+  @array.each do |row|
+  puts row.join
+  end
+end
+
+  
+  def blur(distance)
+    changes = []
+    @array.each_with_index do |row, row_index|
+      row.each_with_index do |cell, cell_index|
+         
+        if cell == 1
+          changes.insert(0, [row_index, cell_index])        
+        end
+      end
+    end 
+    changes.each do |item|
+      above = item[0] - 1
+      @array[above][item[1]] = 1
+      below = item[0] + 1
+      @array[below][item[1]] = 1 
+      left = item[0]
+      @array[left][item[1] + 1] = 1 
+      right = item[0]
+      @array[right][item[1] - 1] = 1 
+    end 
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 0, 1, 0],
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 0]
+])
+image.output_image
+puts "----"
+image.blur(1)
+image.output_image
+
+
